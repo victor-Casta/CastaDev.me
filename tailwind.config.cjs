13ff2b748773bb7ecdd3597e0cf5978f0b5f8ec2
Victor Casta <victorarroyavecastaneda@gmail.com>
2024-03-04 16:04:19 -0500

    update: new library animations
    insert new animations in my page

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+import animations from '@midudev/tailwind-animations'
+
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
@@ -15,5 +17,6 @@ module.exports = {
 	},
 	plugins: [
 		require('tailwindcss-debug-screens'),
+		animations,
 	],
 }
